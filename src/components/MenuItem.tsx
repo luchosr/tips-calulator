3f035f209589e98afdd602da6687fdce80b5c9e7
Luciano Ramello <luchosr@gmail.com>
2024-08-23 12:02:07 +0200

    feat: adds event handler

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,11 +2,15 @@ import type { MenuItem } from '../types';
 
 type MenuItemProps = {
   item: MenuItem;
+  addItem: (item: MenuItem) => void;
 };
 
-export default function MenuItem({ item }: MenuItemProps) {
+export default function MenuItem({ item, addItem }: MenuItemProps) {
   return (
-    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between rounded">
+    <button
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between rounded"
+      onClick={() => addItem(item)}
+    >
       <p>{item.name}</p>
       <p className="font-black">$ {item.price}</p>
     </button>
