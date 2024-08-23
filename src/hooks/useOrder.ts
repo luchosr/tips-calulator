e12c40eaa14e6da0d8596835dfa18992c89ba1eb
Luciano Ramello <luchosr@gmail.com>
2024-08-23 10:54:18 +0200

    feat: Adds custom hook

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -3,8 +3,12 @@ import type { OrderItem } from '../types';
 
 export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
-  const [total, setTotal] = useState(0);
-  const [auth, setAuth] = useState(false);
 
-  return {};
+  const addItem = () => {
+    console.log('agregando...');
+  };
+
+  return {
+    addItem,
+  };
 }
