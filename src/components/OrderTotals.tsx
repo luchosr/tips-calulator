9696a131e66680cc8b563975d33b37a7a9ddf464
Luciano Ramello <luchosr@gmail.com>
2024-08-25 16:51:34 +0200

    feat: adds total payment calculation

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,15 +1,23 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 import type { OrderItem } from '../types';
 import { formatCurrency } from '../helpers';
 
 type OrderTotalProps = {
   order: OrderItem[];
+  tip: number;
 };
 
-export default function OrderTotals({ order }: OrderTotalProps) {
+export default function OrderTotals({ order, tip }: OrderTotalProps) {
   const subtotalAmount = useMemo(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order]
+  );
+
+  const tipAmount = useMemo(() => subtotalAmount * tip, [tip, subtotalAmount]);
+
+  const totalPayment = useMemo(
+    () => tipAmount + subtotalAmount,
+    [tipAmount, subtotalAmount]
   );
 
   return (
@@ -21,10 +29,12 @@ export default function OrderTotals({ order }: OrderTotalProps) {
           <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
         </p>
         <p>
-          Propina: <span className="font-bold">$0</span>
+          Propina:{' '}
+          <span className="font-bold">{formatCurrency(tipAmount)}</span>
         </p>
         <p>
-          Total a pagar: <span className="font-bold">$0</span>
+          Total a pagar:{' '}
+          <span className="font-bold">{formatCurrency(totalPayment)}</span>
         </p>
       </div>
       <button></button>
