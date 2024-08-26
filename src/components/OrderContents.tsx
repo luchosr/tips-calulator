38a39753b83cce636f5520347372f163bab6f162
Luciano Ramello <luchosr@gmail.com>
2024-08-26 08:49:29 +0200

    refactor: new order contents component  structure

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -13,32 +13,28 @@ export default function OrderContents({
   return (
     <div>
       <div className="space-y-3 mt-10">
-        {order.length === 0 ? (
-          <p className="text-center">La orden está vacía</p>
-        ) : (
-          order.map((item) => (
-            <div
-              key={item.id}
-              className="flex justify-between border-t items-center border-gray-200 py-5 last-of-type:border-b"
-            >
-              <div className="">
-                <p className="text-lg">
-                  {item.name} - {formatCurrency(item.price)}
-                </p>
-                <p className="font-black">
-                  Cantidad: {item.quantity} -
-                  {formatCurrency(item.price * item.quantity)}
-                </p>
-              </div>
-              <button
-                className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                onClick={() => removeItem(item.id)}
-              >
-                X
-              </button>
+        {order.map((item) => (
+          <div
+            key={item.id}
+            className="flex justify-between border-t items-center border-gray-200 py-5 last-of-type:border-b"
+          >
+            <div className="">
+              <p className="text-lg">
+                {item.name} - {formatCurrency(item.price)}
+              </p>
+              <p className="font-black">
+                Cantidad: {item.quantity} -
+                {formatCurrency(item.price * item.quantity)}
+              </p>
             </div>
-          ))
-        )}
+            <button
+              className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+              onClick={() => removeItem(item.id)}
+            >
+              X
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
