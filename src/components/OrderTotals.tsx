7e297eee3c9b72520bb37c8925bfa3a3f1f92f6e
Luciano Ramello <luchosr@gmail.com>
2024-08-26 08:48:42 +0200

    feat: improves Order Totals props

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -5,9 +5,14 @@ import { formatCurrency } from '../helpers';
 type OrderTotalProps = {
   order: OrderItem[];
   tip: number;
+  placeOrder: () => void;
 };
 
-export default function OrderTotals({ order, tip }: OrderTotalProps) {
+export default function OrderTotals({
+  order,
+  tip,
+  placeOrder,
+}: OrderTotalProps) {
   const subtotalAmount = useMemo(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order]
@@ -15,7 +20,7 @@ export default function OrderTotals({ order, tip }: OrderTotalProps) {
 
   const tipAmount = useMemo(() => subtotalAmount * tip, [tip, subtotalAmount]);
 
-  const totalPayment = useMemo(
+  const totalAmount = useMemo(
     () => tipAmount + subtotalAmount,
     [tipAmount, subtotalAmount]
   );
@@ -34,10 +39,16 @@ export default function OrderTotals({ order, tip }: OrderTotalProps) {
         </p>
         <p>
           Total a pagar:{' '}
-          <span className="font-bold">{formatCurrency(totalPayment)}</span>
+          <span className="font-bold">{formatCurrency(totalAmount)}</span>
         </p>
       </div>
-      <button></button>
+      <button
+        className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
+        disabled={totalAmount === 0}
+        onClick={placeOrder}
+      >
+        Guardar Orden
+      </button>
     </>
   );
 }
