495df81a3ac09db5da06683585446b3de0add649
Luciano Ramello <luchosr@gmail.com>
2024-08-31 11:34:25 +0200

    feat: implements action dispatcher

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,15 +1,16 @@
+import { OrderActions } from '../reducers/order-reducer';
 import type { MenuItem } from '../types';
 
 type MenuItemProps = {
   item: MenuItem;
-  addItem: (item: MenuItem) => void;
+  dispatch: React.Dispatch<OrderActions>;
 };
 
-export default function MenuItem({ item, addItem }: MenuItemProps) {
+export default function MenuItem({ item, dispatch }: MenuItemProps) {
   return (
     <button
       className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between rounded"
-      onClick={() => addItem(item)}
+      onClick={() => dispatch({ type: 'add-item', payload: { item } })}
     >
       <p>{item.name}</p>
       <p className="font-black">$ {item.price}</p>
