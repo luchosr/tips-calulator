d946d959e9bcebd8956f750765f44ff3a5fe90f3
Luciano Ramello <luchosr@gmail.com>
2024-09-02 22:42:38 +0200

    feat: adds place order dispatcher

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,18 +1,15 @@
 import { useMemo } from 'react';
 import type { OrderItem } from '../types';
 import { formatCurrency } from '../helpers';
+import { OrderActions } from '../reducers/order-reducer';
 
 type OrderTotalProps = {
   order: OrderItem[];
   tip: number;
-  placeOrder: () => void;
+  dispatch: React.Dispatch<OrderActions>;
 };
 
-export default function OrderTotals({
-  order,
-  tip,
-  placeOrder,
-}: OrderTotalProps) {
+export default function OrderTotals({ order, tip, dispatch }: OrderTotalProps) {
   const subtotalAmount = useMemo(
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order]
@@ -45,7 +42,7 @@ export default function OrderTotals({
       <button
         className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
         disabled={totalAmount === 0}
-        onClick={placeOrder}
+        onClick={() => dispatch({ type: 'place-order' })}
       >
         Guardar Orden
       </button>
